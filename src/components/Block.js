b7ce46ba3bd1bb4ab64818709b52acb97ba3f051
LukasevicProjects <lukasevic.robert@gmail.com>
2021-04-07 15:05:58 +0300

    winner imges and  gifs added

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import "./styles/styles.css";
 import leo from "../img/win.gif";
+import notbad from "../img/alecbaldwin.gif";
+import successKid from "../img/SuccessKid.jpg";
 
 
 
 
-function Block({ imageObj, fatherHandleClick, imgIndex, isFlipped, defaultImage }) {
+function Block({ imageObj, fatherHandleClick, imgIndex, isFlipped, defaultImage, win, imgArray }) {
     const [img, setImg] = useState(defaultImage);
+    const [now, setNow] = useState(false);
     
     const setImage = () => {
         setImg(imageObj.image);
     };
+
+    useEffect(() => {
+        if(win){
+            // setImg(<img src={leo} alt="leo"></img>)  
+            setTimeout(() => setImg(imgArray.length === 24 ? <img src={leo} alt="leo"></img> : imgArray.length === 16 ? <img src={notbad} alt="not-bad"></img> : <img src={successKid} alt="success-kid"></img>), 1500);
+            setTimeout(() => setNow(win), 1500);
+        }
+    },[win])
 
     useEffect(() => {
         if (isFlipped) {
@@ -26,12 +37,11 @@ function Block({ imageObj, fatherHandleClick, imgIndex, isFlipped, defaultImage 
 
     return (
 
-        <div onClick={!isFlipped ? handleClick : null} className={`root ${isFlipped ? "flipped" : ""} `}>
+        <div onClick={!isFlipped ? handleClick : null} className={`root ${now ? "" : isFlipped ? "flipped" : ""} `}>
 
             <div className="inner">
-                <div className={isFlipped ? 'front' : 'back'}>
-                    
-                    <img src={leo}></img>
+                <div className={now ? 'back' :  isFlipped ? 'front' : 'back'}>
+                    {img}              
                 </div>
             </div>
         </div>
